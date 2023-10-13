e52608f58328bcfd485d49042a68b2846988fb27
Jory Adair <joryadair@gmail.com>
2023-10-13 07:55:03 -0400

    remove ef core, update packages

--- Docs.Api/Program.cs
+++ Docs.Api/Program.cs
@@ -1,15 +1,10 @@
-using Docs.Api.Data;
 using Docs.Api.Interfaces;
 using Docs.Api.Profiles;
 using Docs.Api.Repositories;
 using Docs.Api.Services;
 using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
-var connectionString = Environment.GetEnvironmentVariable("DefaultConnection", EnvironmentVariableTarget.Process);
-var cosmosConnectionString = Environment.GetEnvironmentVariable("CosmosConnection", EnvironmentVariableTarget.Process);
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults(worker => worker.UseNewtonsoftJson())
@@ -22,10 +17,6 @@ var host = new HostBuilder()
         services.AddScoped(typeof(IPagingService<,>), typeof(PagingService<,>));
         services.AddScoped(typeof(ICachingService<>), typeof(CachingService<>));
         services.AddMemoryCache();
-        services.AddDbContext<AppDbContext>(options =>
-        {
-            options.UseSqlServer(connectionString);
-        });
     })
     .Build();
 
